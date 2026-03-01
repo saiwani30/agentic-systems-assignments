4276b4312f7ed47c11d15f4352848243479a740e
saiwani30 <saiw51012@gmail.com>
2026-03-01 13:32:20 +0530

    Adding solutions to 3 qs in 3 different files

--- qs1.py
+++ qs1.py
@@ -3,7 +3,6 @@ try:
     num1 = input("Enter first number: ")
     num2 = input("Enter second number: ")
 
-    # Convert inputs to integers
     num1 = int(num1)
     num2 = int(num2)
 
@@ -11,10 +10,7 @@ try:
     if num2 == 0:
         print("Cannot divide by zero")
     else:
-        # Print sum
         print("Sum:", num1 + num2)
-        
-        # Print division result
         print("Division:", num1 / num2)
 
 except ValueError:
